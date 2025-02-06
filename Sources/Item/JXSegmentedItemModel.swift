85dc6cf35de3dddddfdb316978e47a9fdfd1699c
Sereivoan Yong <sereivoanyong@gmail.com>
2025-02-06 17:30:49 +0700

    Fix

--- Sources/Item/JXSegmentedItemModel.swift
+++ Sources/Item/JXSegmentedItemModel.swift
@@ -8,24 +8,18 @@
 import Foundation
 import Kingfisher
 
-open class __SegmentedTitleImageItemModel: JXSegmentedTitleItemModel {
-
-  open var imageSource: Source?
-
-  open var selectedImageSource: Source?
-
-  open var titleImageType: JXSegmentedTitleImageType = .leftImage
-
-  open var imageSize: CGSize = .zero
-
-  open var titleImageSpacing: CGFloat = 0
-}
-
-open class __SegmentedImageItemModel: JXSegmentedBaseItemModel {
-
-  open var imageSource: Source?
-
-  open var imagePlaceholder: Placeholder?
-
-  open var imageSize: CGSize = .zero
-}
+//open class __SegmentedTitleImageItemModel: JXSegmentedTitleImageItemModel {
+//
+//  open var imageSource: Source?
+//
+//  open var selectedImageSource: Source?
+//}
+//
+//open class __SegmentedImageItemModel: JXSegmentedBaseItemModel {
+//
+//  open var imageSource: Source?
+//
+//  open var imagePlaceholder: Placeholder?
+//
+//  open var imageSize: CGSize = .zero
+//}
